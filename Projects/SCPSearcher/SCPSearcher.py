eea8cc4333493a37fa54aa6c59048f765ca0948c
Will <williamwsantosa@gmail.com>
2021-12-26 15:46:52 -0800

    Added SCPSearcher functionality to run bot and say hello to the user

--- Projects/SCPSearcher/SCPSearcher.py
+++ Projects/SCPSearcher/SCPSearcher.py
@@ -7,6 +7,19 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("We have logged in as {0.user}".format(client))
+    print("Logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f"{username}: {user_message} ({channel})")
+
+    if message.author == client.user:
+        return
+    
+    if user_message.lower() == "hello":
+        await message.channel.send(f'Hello {username}!')
 
 client.run(TOKEN)
