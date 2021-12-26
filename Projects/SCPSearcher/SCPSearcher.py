6974204b309deec7d5221d9bc2754d9f6e08ad9b
Will <williamwsantosa@gmail.com>
2021-12-26 03:02:54 -0800

    Edited gitignore and added to SCPSearcher to login

--- Projects/SCPSearcher/SCPSearcher.py
+++ Projects/SCPSearcher/SCPSearcher.py
@@ -1,6 +1,12 @@
 import discord
 import random
-import config
+import config as cfg
 
-TOKEN = config.TOKEN
+TOKEN = cfg.TOKEN
 client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+client.run(TOKEN)
