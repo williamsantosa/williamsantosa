e220bb2779ac65a8d017d0e3b131fb4b61e65ba1
Will <williamwsantosa@gmail.com>
2021-12-26 20:32:54 -0800

    Completed team sort, will add leagueSort

--- Projects/ClashHelper/ClashHelper.py
+++ Projects/ClashHelper/ClashHelper.py
@@ -11,6 +11,8 @@ async def on_ready():
     print("Logged in as {0.user}".format(client))
 
 # Helper functions
+
+# 10 players separated into 2 groups of 5
 def team10(players):
     p_copy = players.copy()
     team1 = []
@@ -24,7 +26,32 @@ def team10(players):
             team2.append(p_copy.pop(index))
         
     return (team1, team2)
+
+# Return a list of the players in each team
+# If uneven, will place into the first team, then the next, etc
+def teamSort(players, n):
+    # Create copy of players
+    p_copy = players.copy()
+
+    # Initialize dictionary containing all the teams and the players
+    teams = {} # Team Number : List of players
+    for i in range(n):
+        teams[i] = []
+
+    # Sort until everything is full
+    currTeam = 0
+    while len(p_copy) > 0:
+        # Put player into team and remove from list of players
+        index = random.randint(0, len(p_copy) - 1)
+        teams[currTeam].append(p_copy.pop(index))
+
+        # Increment and reset if needed
+        currTeam = currTeam + 1
+        if currTeam >= n:
+            currTeam = 0
         
+    return teams
+
 # Message is sent into a channel response(s)
 @client.event
 async def on_message(message):
