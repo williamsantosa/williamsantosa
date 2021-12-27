c941bbdfa7dd1dbed1e4bc7c9ebb21df51d83b68
Will <williamwsantosa@gmail.com>
2021-12-26 17:55:36 -0800

    Edited SCPSearcher README and program

--- Projects/SCPSearcher/README.md
+++ Projects/SCPSearcher/README.md
@@ -2,19 +2,30 @@ SCP Searcher
 -------------------
 DESCRIPTION
 
-
+SCPs 
 
 -------------------
 FILES
 
 - SCPSearcher.py
 
+Contains the code required to run a bot that returns the SCP wiki page for specified SCP.
+
 - config.py
 
-Contains the parameters required to run SCPSearcher.py 
+Contains the parameters required to run SCPSearcher.py.
 
 -------------------
-INSTRUCTION
+INSTRUCTIONS
 
-1. Open terminal
-2. Enter ~/SCPSearcher repository
+1. Create a Discord server (https://support.discord.com/hc/en-us/articles/204849977-How-do-I-create-a-server-)
+2. Create a Discord application (https://discord.com/developers/applications)
+3. Create a Bot for the application (Application_Name -> Bot)
+4. Generate the invite link for the bot into your Discord server (Application_Name -> OAuth2 -> Scopes -> Check Bot -> Check Administrator -> Copy Link)
+5. Copy and paste link into browser and invite the bot into your Discord server
+6. Copy the token for the Discord bot (Application_Name -> Bot -> Token -> Copy)
+7. Open terminal
+8. Enter ~/SCPSearcher repository
+9. Create a python file called "config.py" and insert "TOKEN = '<Discord Bot Token>'" within the file
+10. Enter "python SCPSearcher.py" into terminal
+11. Send message !help in the discord server with the bot
--- Projects/SCPSearcher/SCPSearcher.py
+++ Projects/SCPSearcher/SCPSearcher.py
@@ -1,34 +1,44 @@
 import discord
-import random
 import config as cfg
 
 TOKEN = cfg.TOKEN
 client = discord.Client()
 
+# Prints the client to terminal
 @client.event
 async def on_ready():
     print("Logged in as {0.user}".format(client))
 
+# Message is sent into a channel response(s)
 @client.event
 async def on_message(message):
     username = str(message.author).split("#")[0]
     user_message = str(message.content)
     channel = str(message.channel.name)
-    print(f"{username}: {user_message} ({channel})")
-
-    if message.author == client.user:
-        return
-    
     lmsg = user_message.lower()
 
+    print(f"{username}: {user_message} ({channel})")
+
+    # Check for if the message sender is the bot and end the program if so
+    if message.author == client.user:
+        return
+
+    # Help command
     if lmsg == "!help":
-        await message.channel.send(f'Hello {username}!')
+        await message.channel.send("Use !scp <number> to return the wiki entry for said SCP")
+        return
+    # Search and return the link for an SCP entry with error check
     elif lmsg.split(" ")[0] == "!scp" and len(lmsg.split(" ")) == 2:
         n = lmsg.split(" ")[1]
+        # Check if entry is valid or not
+        try:
+            int(n)
+        except ValueError:
+            return
+        if int(n) < 0:
+            n = n[1:]
         await message.channel.send(f"https://scp-wiki.wikidot.com/scp-{n}")
+        return
 
-@client.event
-async def shutdown(context):
-    exit()
-
+# Run the client on server/machine
 client.run(TOKEN)
